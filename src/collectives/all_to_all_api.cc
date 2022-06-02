5cf5aadae6a5666679f8f11e78102e2d206c04c9
Yifan Xiong <yifan.xiong@microsoft.com>
2022-06-02 19:15:20 +0800

    Init 2DH alltoall algorithm
    Initial 2D hierarchical alltoall algorithm.

--- src/collectives/all_to_all_api.cc
+++ src/collectives/all_to_all_api.cc
@@ -9,6 +9,8 @@
 #include "collectives.h"
 #include "graph/topo.h"
 
+hipError_t strideMemcpyAsync(void *dst, const void *src, const size_t size, const int height, const int width, hipStream_t stream);
+
 NCCL_API(ncclResult_t, ncclAllToAll, const void* sendbuff, void* recvbuff, size_t count, ncclDataType_t datatype,
   ncclComm_t comm, hipStream_t stream);
 ncclResult_t ncclAllToAll(const void* sendbuff, void* recvbuff, size_t count, ncclDataType_t datatype,
@@ -25,12 +27,39 @@ ncclResult_t ncclAllToAll(const void* sendbuff, void* recvbuff, size_t count, nc
     NCCLCHECK(ncclCommCount(comm, &nRanks));
     size_t rankOffset = count * ncclTypeSize(datatype);
     if (count == 0) return ncclSuccess;
+    /*
     NCCLCHECK(ncclGroupStart());
     for (int r=0; r<nRanks; r++) {
       NCCLCHECK(ncclSend(((char*)sendbuff)+r*rankOffset, count, datatype, r, comm, stream));
       NCCLCHECK(ncclRecv(((char*)recvbuff)+r*rankOffset, count, datatype, r, comm, stream));
     }
     NCCLCHECK(ncclGroupEnd());
+    */
+    int nGpus = comm->localRanks, nNodes = comm->nNodes;
+    if (nGpus == 1 || nNodes == 1) {
+      WARN("number of local GPUs (%d) or number of nodes (%d) is 1.", nGpus, nNodes);
+      return ncclInvalidUsage;
+    }
+    // 2D Hierarchical AlltoAll algorithm
+    // phase 0. per-gpu (nGpus) stride copy
+    CUDACHECK(strideMemcpyAsync(recvbuff, sendbuff, rankOffset, nGpus, nNodes, stream));
+    // phase 1. intra-node alltoall
+    NCCLCHECK(ncclGroupStart());
+    for (int g = 0; g < nGpus; g++) {
+      NCCLCHECK(ncclSend(((char*)recvbuff) + g * nNodes * rankOffset, nNodes * count, datatype, g + comm->node * nGpus, comm, stream));
+      NCCLCHECK(ncclRecv(((char*)sendbuff) + g * nNodes * rankOffset, nNodes * count, datatype, g + comm->node * nGpus, comm, stream));
+    }
+    NCCLCHECK(ncclGroupEnd());
+    // phase 2. per-gpu (nNodes) stride copy
+    CUDACHECK(strideMemcpyAsync(recvbuff, sendbuff, rankOffset, nNodes, nGpus, stream));
+    // phase 3. inter-node alltoall
+    NCCLCHECK(ncclGroupStart());
+    for (int n = 0; n < nNodes; n++) {
+      NCCLCHECK(ncclSend(((char*)recvbuff) + n * nGpus * rankOffset, nGpus * count, datatype, n * nGpus + comm->cudaDev, comm, stream));
+      NCCLCHECK(ncclRecv(((char*)sendbuff) + n * nGpus * rankOffset, nGpus * count, datatype, n * nGpus + comm->cudaDev, comm, stream));
+    }
+    NCCLCHECK(ncclGroupEnd());
+    CUDACHECK(hipMemcpyAsync(recvbuff, sendbuff, comm->nRanks * rankOffset, hipMemcpyDeviceToDevice, stream));
     return ncclSuccess;
   }
 }
